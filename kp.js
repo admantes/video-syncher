09e9d675ffba0e4d5d34ac5c791e5571fa6afd59
Aris Mantes <admantes@gmail.com>
2019-05-07 16:52:35 +0800

    added kp sample

---
+++ kp.js
@@ -0,0 +1,7 @@
+﻿var kpArr = ["This presentation only provides basic First Aid knowledge and about developing your understanding of basic First Aid:", "To preserve life;", "To protect the unconscious;", "To prevent the condition from worsening;", "To promote recovery; and", "To provide reassurance and comfort to the ill or injured until proper medical assistance arrives."];
+
+var kpTrackArr = [2.17,9.96,11.46,13.25,15.83,18.00];
+
+var kpIndentArr = [0,1,1,1,1,1];
+
+var bulletType = [1,1,1,1,1,1];
